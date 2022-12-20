b8ae0a96e3b3a5a62fcffd24fdbec7c0e2148658
codeCampWithSifat <sayedhossainsifat300@.com>
2022-12-21 00:52:15 +0600

    Done

--- index.js
+++ index.js
@@ -4,10 +4,27 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 // use all the middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.AUTH_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden" });
+    }
+    // console.log("decoded", decoded);
+    req.decoded = decoded;
+    next();
+  });
+}
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.nrvwj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -54,21 +71,36 @@ async function run() {
     });
 
     // see a volantear by filtering email
-    app.get("/volantear", async (req, res) => {
-        const email = req.query.email;
-        const query = {email : email};
+    app.get("/volantear", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      console.log(decodedEmail)
+      if (email === decodedEmail) {
+        const query = { email: email };
         const cursor = addVolantear.find(query);
         const result = await cursor.toArray();
         res.send(result);
-      });
+      } else {
+        res.status(403).send({message : "Forbidden"})
+      }
+    });
 
-    // delete a volantear 
-    app.delete('/allvolantear/:id',async(req,res) => {
-       const id = req.params.id ;
-       const query = {_id : ObjectId(id)};
-       const result = await addVolantear.deleteOne(query);
-       res.send(result);
-    })
+    // CREATE AUTH TOKEN
+    app.post("/token", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.AUTH_TOKEN, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+
+    // delete a volantear
+    app.delete("/allvolantear/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await addVolantear.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
